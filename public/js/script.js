bee520563ef9aa534e439e32e18397cdb6fadb1b
swaroop <swarooppatnaik123@gmail.com>
2023-01-07 23:23:45 +0530

    script updated

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,44 @@
-console.log("Swaroop's Todo Manager");
+/* eslint-disable no-unused-vars */
+var token = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content");
+
+// Function to check or uncheck a todo item
+function updateTodo(id) {
+  const boolVal = document.getElementById("todos-checkbox-" + id).checked;
+  fetch(`/todos/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      _csrf: token,
+      completed: boolVal,
+    }),
+  })
+    .then((res) => {
+      if (res.ok) {
+        window.location.reload();
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+// Function to delete a todo item
+function deleteTodo(id) {
+  fetch(`/todos/${id}`, {
+    method: "delete",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      _csrf: token,
+    }),
+  })
+    .then((res) => {
+      if (res.ok) {
+        window.location.reload();
+      }
+    })
+    .catch((err) => console.log(err));
+}
